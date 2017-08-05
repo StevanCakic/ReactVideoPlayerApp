3b4b11b8787246de6cf7b79866c3fa6714f82174
StevanCakic <stevancakic93@gmail.com>
2017-08-05 17:25:45 +0200

    completed simple video playlist app connected with YT API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from "react";
+import _ from "lodash";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetails from "./components/video_details";
 
 const API_KEY = "AIzaSyCKwzbzpFKHhf5qin3cKBJE0vyoG2WDfIQ";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("witcher 3 music");
+  }
+
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term}, videos => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(term =>{
+      this.videoSearch(term)
+    }, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
